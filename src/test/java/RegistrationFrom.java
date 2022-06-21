1fd16c2392844b0db44297ae3da20cfaf5e17d69
Radmir <yaradmir2203@gmail.com>
2022-06-21 22:48:02 +0500

    Add test2

--- src/test/java/RegistrationFrom.java
+++ src/test/java/RegistrationFrom.java
@@ -10,6 +10,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 
 public class RegistrationFrom {
@@ -22,35 +23,41 @@ public class RegistrationFrom {
 
     @Test
     void successfulTest() {
+        step("Заходим на сайт demoqa", () -> {
+            open("/automation-practice-form");
+        });
 
-        open("/automation-practice-form");
+        step("Вводим данные в форму", () -> {
+            $("[id=firstName]").setValue("Alex");
+            $("[id=lastName]").setValue("Larin");
+            $("[id=userEmail]").setValue("alexLar@gmail.com");
+            $("#genterWrapper").$(byText("Male")).click();
+            $("[id=userNumber]").setValue("3748596032");
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").click();
+            $(".react-datepicker__month-select").selectOption("April");
+            $(".react-datepicker__year-select").click();
+            $(".react-datepicker__year-select").selectOption("1990");
+            $(".react-datepicker__day--010:nth-child(3)").click();
+            $("#subjectsInput").setValue("Physics").pressEnter();
+            $("#hobbiesWrapper").$(byText("Reading")).click();
+            $("#uploadPicture").uploadFile (new File("src/test/resources/png.png"));
+            $("#currentAddress-wrapper").click();
+            $("#currentAddress").val("Lenina 5");
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText("NCR")).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText("Delhi")).click();
+            $("#submit").click();
+        });
 
-        $("[id=firstName]").setValue("Alex");
-        $("[id=lastName]").setValue("Larin");
-        $("[id=userEmail]").setValue("alexLar@gmail.com");
-        $("#genterWrapper").$(byText("Male")).click();
-        $("[id=userNumber]").setValue("3748596032");
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").click();
-        $(".react-datepicker__month-select").selectOption("April");
-        $(".react-datepicker__year-select").click();
-        $(".react-datepicker__year-select").selectOption("1990");
-        $(".react-datepicker__day--010:nth-child(3)").click();
-        $("#subjectsInput").setValue("Physics").pressEnter();
-        $("#hobbiesWrapper").$(byText("Reading")).click();
-        $("#uploadPicture").uploadFile (new File("src/test/resources/png.png"));
-        $("#currentAddress-wrapper").click();
-        $("#currentAddress").val("Lenina 5");
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText("NCR")).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText("Delhi")).click();
-        $("#submit").click();
+        step("Проверяем итог", () -> {
+            $(".modal-content").shouldHave(text("Thanks for submitting the form"),
+                    text("alexLar@gmail.com"),
+                    text("alexLar@gmail.com"), text("Male"), text("3748596032"), text("Lenina 5"),
+                    text("10 April,1990"), text("Physics"), text("Reading"),
+                    text("png.png"), text("Lenina 5"), text("NCR Delhi"));
+        });
 
-        $(".modal-content").shouldHave(text("Thanks for submitting the form"),
-                text("alexLar@gmail.com"),
-                text("alexLar@gmail.com"), text("Male"), text("3748596032"), text("Lenina 5"),
-                text("10 April,1990"), text("Physics"), text("Reading"),
-                text("png.png"), text("Lenina 5"), text("NCR Delhi"));
     }
 }
